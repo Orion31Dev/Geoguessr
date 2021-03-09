b5fe200ed6687cdd9f50e8509aac4cce9c4ebec6
Orion31Dev <orion31dev@gmail.com>
2021-03-09 16:03:46 -0500

    fix process is not defined

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,8 +15,8 @@ module.exports = merge(commonConfig, {
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
     }),
   ],
 });
